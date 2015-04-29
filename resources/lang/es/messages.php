d826b7ebfad6ec370540fee684f7a851989d8657
Alejandro Feher Canadell <alejandrofeher@gmail.com>
2015-04-29 12:21:36 +0200

    se sta liando de commits esto (solo messages.php)

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -1,17 +1,9 @@
 <?php
 
 return [
-
-	/*
-	|--------------------------------------------------------------------------
-	| Validation Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines contain the default error messages used by
-	| the validator class. Some of these rules have multiple versions such
-	| as the size rules. Feel free to tweak each of these messages here.
-	|
-	*/
-
-	"accepted"             => "The :attribute must be accepted.",
-	];
+	'title'=>'Poblaciones',
+	'noPoblations'=>'No tienes ninguna población!',
+	'create'=>'Crea una población.',
+	'edit'=>'Edita',
+	'delete'=>'Elimina',
+];
