e1146e5243ba682a5b417ccec35f801f00eb12bb
Alejandro Feher Canadell <alejandrofeher@gmail.com>
2015-04-29 13:30:35 +0200

    resources/lang/en/alerts.php

---
+++ resources/lang/en/alerts.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'title'=>'Poblations',
+	'noPoblations'=>'You don\'t have any poblation!',
+	'create'=>'Create a poblation.',
+	'edit'=>'Edit',
+	'delete'=>'Delete',
+	'noVotants'=>'You don\'t have any votant!',
+	'createVotant'=>'Create a votant.',
+	'backPoblations'=>'Go back to the poblations menu.'
+];
